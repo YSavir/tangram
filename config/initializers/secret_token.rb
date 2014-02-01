c0bc04230e498df697311e13be62c5432789bc40
Yaniv Savir <saviry@gmail.com>
2014-02-01 11:55:57 -0500

    base rails app setup, index.html removed, username removed from database.yml

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tanagram::Application.config.secret_token = '4c2bb5d7e0d81e7496cde2da43d48a50c8ffee41f2ffd261dac2b61a1216e28d8ee76d21995cd562c71a48fc433e07b52526a784f53a4a6b5a110bde61972306'
